36247391edfc5082172ccc60ba2d24d6a9d35272
likhithkp <likhithkpdev@gmail.com>
2024-07-31 14:29:09 +0530

    Server

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello")
+/**
+ * Data Storage
+ *  In simple words as per the definiton, its a place where we store data which can be of any type(Video, Binary etc).
+ *  
+ * Types of DBS
+ *  SQL & NoSQL
+ *  SQL: Structured query language
+ *  Data is stored in a structured format, mainly in table format. It has rows and columns.
+ *  Users Collection
+ *  +---------------------------------------------------------------+
+    | user_id  | name      | email          | password  | address   |
+    |---------------------------------------------------------------|
+    | String   | String    | String         | String    | Object    |
+    |---------------------------------------------------------------|
+    | Address Object:                                           |
+    |---------------------------------------------------------------|
+    | street   | city      | state          | zip       | country   |
+    |---------------------------------------------------------------|
+    | String   | String    | String         | String    | String    |
+    +---------------------------------------------------------------+
+ * 
+ *  NoSQL:
+ *  Data is stored in object format. 
+ *  {
+ *      "name" : "Likhith",
+ *      "age"  : 23,
+ *      "gender" : "male"
+ *  }
+ *
+ * What and Why
+ * Terminologies - collection, documents, schmas, keys, models
+ */
